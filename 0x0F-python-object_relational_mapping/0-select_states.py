f3984678f68179778caf9839237eaf5f7dbfab7f
Ayo Adekunle <ay4eva2006@gmail.com>
2023-03-19 01:32:46 -0700

    task 0 error fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db = sys.argv[3]
     db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=db, port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
-    rows = cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
+    cur.close()
     db.close()

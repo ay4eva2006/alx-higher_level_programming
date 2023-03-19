de2b9a2f8170139df5fc51daa6f641333e44ace2
Ayo Adekunle <ay4eva2006@gmail.com>
2023-03-19 11:52:34 -0700

    my tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-
-# MySQL database configuration
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-# Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
-
-# Create a cursor object
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-# Execute the SQL query to select states with name starting with 'N'
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC""")
-
-# Fetch all the rows in a list
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
-
-# Print the results
     for row in rows:
         print(row)
-
-# Close the cursor and database connection
     cur.close()
     db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,45 +1,17 @@
 #!/usr/bin/python3
-"""
-This script retrieves data from the 'states' table in a safe way,
-using the LIKE operator to match the state name, and removes the repetition.
-"""
-
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    # Check if all arguments are present
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
-        exit(1)
-
-    # Get arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to MySQL database
-    db = MySQLdb.connect(host="localhost",
-                         user=username,
-                         passwd=password,
-                         db=db_name,
-                         port=3306)
-
-    # Create a cursor object
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-    # Execute the SQL query in a safe way and remove repetition
-    cur.execute("SELECT DISTINCT * FROM states WHERE name LIKE %s", (state_name,))
-
-    # Fetch all the rows
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = cur.fetchall()
-
-    # Print the results
     for row in rows:
         print(row)
-
-    # Close cursor and database connection
     cur.close()
     db.close()

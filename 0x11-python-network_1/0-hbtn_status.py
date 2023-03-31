540e0a7946bffcdb232b937a863481a20b9d1212
Ayo Adekunle <ay4evA2006@gmail.com>
2023-03-31 11:19:25 -0700

    fix pep8 error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,8 @@
 - fetches https://alx-intranet.hbtn.io/status.
 - uses urlib package
 """
+
+
 import urllib.request
 
 
@@ -10,7 +12,7 @@ def main():
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         html = res.read()
 
-    print("Body res:")
+    print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
     print("\t- utf8 content: {}".format(html.decode('utf-8')))

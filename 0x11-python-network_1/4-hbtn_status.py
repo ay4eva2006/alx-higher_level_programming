635df8fcda34101732b34bd2c11b4bde02b8d59b
Ayo Adekunle <ay4evA2006@gmail.com>
2023-03-31 12:28:05 -0700

    task 4 & 5

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    body = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
